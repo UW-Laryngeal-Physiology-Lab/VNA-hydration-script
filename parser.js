89d394ef150dabbc064b1777a6d656f6f0a57052
jhersbo <jhersbo@gmail.com>
2021-10-27 23:09:41 -0500

    added some comments

--- parser.js
+++ parser.js
@@ -19,11 +19,11 @@ fileInput.addEventListener('change', function(e){
     reader.readAsText(fileInput.files[0])
 }, false)
 
-//.csv files are encoded slightly differently on mac vs. Windows. Therefore, we have to have a function that will get the client machine's OS. The csvToArray and formatArray functions will run slightly differently depending on 
+//.csv files are encoded slightly differently on Mac vs. Windows. Therefore, we have to have a function that will get the client machine's OS. The csvToArray and formatArray functions will run slightly differently depending on the OS. For now, it is going to be compatible with Windows and Mac OS.
 
-
-
-
+function operatingSystemDetector(){
+    //I want this to be able to detect '\r' in the array string that it is passed (i think .match()? or something). I think it will descriminate against the Mac machine. It then can return a true boolean and be used to modulate the csvToArray function to format the array for Windows machines. 
+}
 
 //csv parser and formatter
 
